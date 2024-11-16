d1d9e6551c193852f86016234a157b67d0276eac
Dash- <dash@dashda.sh>
2024-11-16 17:15:22 -0300

    readd normalize.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Footer from './components/footer'
+import './normalize.css'
 import './index.css'
 import './main.css'
 import './var.css'
