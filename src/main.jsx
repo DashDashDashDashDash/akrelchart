e34ec8959d754d2c14b916bf4745b62d4f2b771c
Dash- <dash@dashda.sh>
2024-11-16 17:16:32 -0300

    whoops, delete leftover index.css from vite preset

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Footer from './components/footer'
 import './normalize.css'
-import './index.css'
-import './main.css'
 import './var.css'
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
