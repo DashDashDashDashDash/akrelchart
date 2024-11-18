72791dfa8ec56f4fa514c7a5fb1d73688d58bd58
Dash- <dash@dashda.sh>
2024-11-17 21:23:03 -0300

    port bare cytoscape graph

---
+++ src/components/maingraph.jsx
@@ -0,0 +1,9 @@
+import CytoscapeComponent from "react-cytoscapejs"
+import './maingraph.css'
+
+
+export default function MainGraph() {
+  return (
+    <CytoscapeComponent id="cy" />
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Footer from './components/footer'
+import MainGraph from './components/maingraph'
 import './normalize.css'
 import './var.css'
 import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <MainGraph />
     <Footer />
   </StrictMode>,
 )
