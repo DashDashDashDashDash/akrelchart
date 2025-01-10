242daa29e1ae08f262c804d6ede4f8bf7b3ab7a8
Dash- <dash@dashda.sh>
2025-01-09 21:39:18 -0300

    backport footer responsiveness

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,14 @@
 import { useState, useContext } from 'react'
 import { CytoscapeContext } from '../cytoscapeContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAnglesDown } from '../assets/icons/footer/faAnglesDown'
+import { faMagnifyingGlass } from '../assets/icons/footer/faMagnifyingGlass'
+import { faTextSize } from '../assets/icons/footer/faTextSize'
+import { faUser } from '../assets/icons/footer/faUser'
+import { faKey } from '../assets/icons/footer/faKey'
+import { faRightFromBracket } from '../assets/icons/footer/faRightFromBracket'
+import { faFilePen } from '../assets/icons/footer/faFilePen'
+import { faCircleQuestion } from '../assets/icons/footer/faCircleQuestion'
 import './footer.css'
 import ptiloBanner from '../assets/images/banner.png'
 
@@ -30,37 +39,77 @@ export default function Footer() {
   // cause it has an animation unlike forms
   return (
     <>
-    <link rel="preload" as="image" src="../assets/images/banner-awake.png"/>
-    { visibility ? null :
-      // no need for restoreui_i anymore also maybe redesign how this shows up
-      // inert bs: https://github.com/facebook/react/issues/17157#issuecomment-1687842532
-      <div className="restoreui_v" id="restoreui" inert={visibility ? '' : undefined}>
-        <a href="#" className="hiddenlink" onClick={() => {toggleVisible()}}>unhide</a>
-      </div>
-    }
-    <div className={visibility ? 'footer_v' : 'footer_i'} id="footer">
-      <nav className="ftr_sub" id="ftr_gen">
-        <img id="banner" src={ptiloBanner}/>
-          <div className="ftr_sep" id="first_sep"></div>
-          <div className="ftr_btn"><a href="#" className="btn_text" onClick={() => {toggleVisible()}}>hide</a></div>
-          <div className="ftr_sep"></div>
-          <div className="ftr_btn"><a href="#" className="btn_text" onClick="popsearch()">search</a></div>
-          <div className="ftr_sep"></div>
-          <div className="ftr_btn"><a href="#" className="btn_text" onClick={() => {toggleTextSize()}}>text size</a></div>
-      </nav>
-      <nav className="ftr_sub ftr_usr">
-        <div id="loginstr" className="admin none">logged in as <span id="username">guest</span></div>
-        <div className="ftr_sep admin none"></div>
-        <div className="ftr_btn admin none"><a href="#" className="btn_text" onClick="showpwchange()" id="pwa">change password</a>
+      <link rel="preload" as="image" src="../assets/images/banner-awake.png" />
+      <link rel="preload" as="image" src="../assets/images/banner-resp.png" />
+      <link rel="preload" as="image" src="../assets/images/banner-resp-awake.png" />
+      {visibility ? null :
+        // no need for restoreui_i anymore also maybe redesign how this shows up
+        // inert bs: https://github.com/facebook/react/issues/17157#issuecomment-1687842532
+        <div className="restoreui_v" id="restoreui" inert={visibility ? '' : undefined}>
+          <a href="#" className="hiddenlink" onClick={() => { toggleVisible() }}>unhide</a>
         </div>
-        <div className="ftr_sep admin none"></div>
-        <div className="ftr_btn admin none"><a href="#" className="btn_text" onClick="logout()" id="logout">logout</a></div>
-        <div className="ftr_sep admin none"></div>
-        <div className="ftr_btn admin"><a href="#" className="btn_text" onClick="showlogin()" id="logina">edit</a></div>
-        <div className="ftr_sep"></div>
-        <div className="ftr_btn"><a href="#" className="btn_text" onClick="showabout()">about</a></div>
-      </nav>
-    </div>
+      }
+      <div className={visibility ? 'footer_v' : 'footer_i'} id="footer">
+        <nav className="ftr_sub" id="ftr_gen">
+          <img id="banner" src={ptiloBanner} />
+          <div className="ftr_sep" id="first_sep"></div>
+          <div className="ftr_btn">
+            <a href="#" className="btn_text" onClick={() => { toggleVisible() }}>
+              <FontAwesomeIcon icon={faAnglesDown} className="navico" />
+              <span className="resp-text">hide</span>
+            </a>
+          </div>
+          <div className="ftr_sep"></div>
+          <div className="ftr_btn">
+            <a href="#" className="btn_text" onClick="popsearch()">
+              <FontAwesomeIcon icon={faMagnifyingGlass} className="navico" />
+              <span className="resp-text">search</span>
+            </a>
+          </div>
+          <div className="ftr_sep"></div>
+          <div className="ftr_btn">
+            <a href="#" className="btn_text" onClick={() => { toggleTextSize() }}>
+              <FontAwesomeIcon icon={faTextSize} className="navico" />
+              <span className="resp-text">text size</span>
+            </a>
+          </div>
+        </nav>
+        <nav className="ftr_sub ftr_usr">
+          <div id="loginstr" className="admin none">
+            <FontAwesomeIcon icon={faUser} className="navico" />
+            <span className="resp-text">logged in as</span>
+            <span id="username">guest</span>
+          </div>
+          <div className="ftr_sep admin none"></div>
+          <div className="ftr_btn admin none">
+            <a href="#" className="btn_text" onClick="showpwchange()" id="pwa">
+              <FontAwesomeIcon icon={faKey} className="navico" />
+              <span className="resp-text">change password</span>
+            </a>
+          </div>
+          <div className="ftr_sep admin none"></div>
+          <div className="ftr_btn admin none">
+            <a href="#" className="btn_text" onClick="logout()" id="logout">
+              <FontAwesomeIcon icon={faRightFromBracket} className="navico" />
+              <span class="resp-text">logout</span>
+            </a>
+          </div>
+          <div className="ftr_sep admin none"></div>
+          <div className="ftr_btn admin">
+            <a href="#" className="btn_text" onClick="showlogin()" id="logina">
+              <FontAwesomeIcon icon={faFilePen} className="navico" />
+              <span className="resp-text">edit</span>
+            </a>
+          </div>
+          <div className="ftr_sep"></div>
+          <div className="ftr_btn">
+            <a href="#" className="btn_text" onClick="showabout()">
+              <FontAwesomeIcon icon={faCircleQuestion} className="navico" />
+              <span className="resp-text">about</span>
+            </a>
+          </div>
+        </nav>
+      </div>
     </>
   )
 }
