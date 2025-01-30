cc7c5738ff2508aca6246b1d1325efbd16583ed0
Dash- <dash@dashda.sh>
2025-01-30 13:40:01 -0300

    set rollupOptions to build from indexv2.html
    once this branch is merged this should go back to index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+//  base: "/v2/"
+  build: {
+    rollupOptions: {
+      input: {
+        main: "indexv2.html"
+      }
+    }
+  }
 })
